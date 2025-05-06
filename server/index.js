6872b4cf8bd1b6b9cf6e90f8e8a746b6927778f9
MuhammadSufian98 <sufianhassan98@gmail.com>
2025-05-06 22:06:31 +0500

    `Update MongoDB connection options in server/index.js`

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(process.env.MONGODB_LINK)
+  .connect(process.env.MONGODB_LINK, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
