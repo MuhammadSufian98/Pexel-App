43470f89b249155c0c768ee39e6918b5cfb516b2
MuhammadSufian98 <sufianhassan98@gmail.com>
2025-05-09 11:34:30 +0500

    `Update MongoDB connection link in server/index.js`

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(process.env.MONGODB_DB_COMPASS_LINK)
+  .connect(process.env.MONGODB_LINK)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
